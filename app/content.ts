10557bdd930d7fe4685babfeea43e3ef4f155ca9
Futa Hirakoba <korosuke613613@gmail.com>
2019-01-04 06:31:07 +0900

    enable class

--- app/content.ts
+++ app/content.ts
@@ -1,33 +1,47 @@
 'use strict';
 
-let title = document.getElementById('productTitle').innerText;
-let author: any = document.getElementById('bylineInfo').getElementsByClassName('a-link-normal')[0];
-author = author.innerText.replace('のAmazon著者ページを見る', '');
-
-let details = document.getElementById('detail_bullets_id').getElementsByTagName('li');
-
-let isbn10 = 'null';
-let isbn13 = 'null';
-for (const index in details) {
-  const text = details[index].innerText;
-  if (typeof text !== 'string') {
-    continue;
+class BookInfo {
+  title: string;
+  author: string;
+  isbn10: string = 'null';
+  isbn13: string = 'null';
+  constructor() {
+    this.setTitle();
+    this.setAuthor();
+    this.setIsbn();
   }
-  if (text.match(/ISBN-10/)) {
-    isbn10 = text;
-  }else if (text.match(/ISBN-13/)) {
-    isbn13 = text;
+
+  setTitle() {
+    this.title = document.getElementById('productTitle').innerText;
+  }
+
+  setAuthor() {
+    const tmpAuthor: any = document.getElementById('bylineInfo').getElementsByClassName('a-link-normal')[0];
+    this.author = tmpAuthor.innerText.replace('のAmazon著者ページを見る', '');
+  }
+
+  setIsbn() {
+    const details = document.getElementById('detail_bullets_id').getElementsByTagName('li');
+
+    for (const index in details) {
+      const text = details[index].innerText;
+      if (typeof text !== 'string') {
+        continue;
+      }
+      if (text.match(/ISBN-10/)) {
+        this.isbn10 = text;
+      }else if (text.match(/ISBN-13/)) {
+        this.isbn13 = text;
+      }
+    }
   }
 }
 
+let bookInfoLocal = new BookInfo();
+console.log(bookInfoLocal);
 chrome.runtime.sendMessage(
-  {
-    title,
-    author,
-    isbn10,
-    isbn13,
-  },
-  (response) => {
-    console.log(response);
-    return true;
-  });
+    bookInfoLocal,
+    (response) => {
+      console.log(response);
+      return true;
+    });
