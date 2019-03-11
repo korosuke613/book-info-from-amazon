68631cb4ccf717c6203605b2a2a5045007bee9ab
Futa Hirakoba <korosuke613613@gmail.com>
2019-03-12 00:51:53 +0900

    add move option page

--- app/popup.ts
+++ app/popup.ts
@@ -1,5 +1,13 @@
 'use strict';
 
+document.querySelector('#go-to-options').addEventListener('click', () => {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
+
 chrome.tabs.query({}, () => {
   const bookInfo = chrome.extension.getBackgroundPage()['bookInfo'];
   const results = document.getElementById('results') as HTMLInputElement;
