dc258ed44af3a5a97692728e4d3907028068d8b1
Futa Hirakoba <korosuke613613@gmail.com>
2019-03-12 00:51:33 +0900

    enable option

--- app/popup.ts
+++ app/popup.ts
@@ -2,11 +2,35 @@
 
 chrome.tabs.query({}, () => {
   const bookInfo = chrome.extension.getBackgroundPage()['bookInfo'];
-  console.log(bookInfo);
-
   const results = document.getElementById('results') as HTMLInputElement;
-  results['value'] = `タイトル: ${bookInfo.title}\n著者: ${bookInfo.author}\n${bookInfo.publisher}\n`;
-  results['value'] += `価格(税込): ${bookInfo.price}\n`;
-  results['value'] += `${bookInfo.isbn10}\n${bookInfo.isbn13}\nURL: ${bookInfo.url}`;
-  results.select();
+  chrome.storage.sync.get(
+    ['title', 'author', 'url', 'price', 'publisher', 'isbn10', 'isbn13'],
+    (item) => {
+      console.log(item);
+      results['value'] = '';
+      if (item.title === 'on') {
+        results['value'] += `タイトル: ${bookInfo.title}\n`;
+      }
+      if (item.author === 'on') {
+        results['value'] += `著者: ${bookInfo.author}\n`;
+      }
+      if (item.publisher === 'on') {
+        results['value'] += `${bookInfo.publisher}\n`;
+      }
+      if (item.price === 'on') {
+        results['value'] += `価格(税込): ${bookInfo.price}\n`;
+      }
+      if (item.isbn10 === 'on') {
+        results['value'] += `${bookInfo.isbn10}\n`;
+      }
+      if (item.isbn13 === 'on') {
+        results['value'] += `${bookInfo.isbn13}\n`;
+      }
+      if (item.url === 'on') {
+        results['value'] += `URL: ${bookInfo.url}`;
+      }
+      results.select();
+
+    });
+
 });
