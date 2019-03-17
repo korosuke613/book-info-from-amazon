59ee527f6935f27c674bab399f3bce5ab9185add
Futa Hirakoba <korosuke613613@gmail.com>
2019-03-18 01:51:40 +0900

    create test

--- update_version.js
+++ update_version.js
@@ -1,24 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const file_path = path.resolve(__dirname, "app", "manifest.json");
+const update = require('../version');
 
-const config = JSON.parse(
-  fs.readFileSync(
-    file_path
-  )
-);
 
-const env = process.argv[2];
-if(typeof env !== 'string'){
-  process.on('exit', () => {
-    process.exit(1);
-  });
-}
-console.log(`next version is ${env}`);
-config.version = env.replace('v', '');
+const main = ()=> {
+  const version = update.load_version(process.argv[2]);
+  if(version === false) {
+    process.on('exit', () => {
+      process.exit(1);
+    });
+  }
+  console.log(`next version is v${version}`);
+  const config = update.load_JSON(file_path);
+  config.version = version;
 
-fs.writeFileSync(
-  file_path,
-  JSON.stringify(config,null,'  '),
-  "utf-8"
-);
+  fs.writeFileSync(
+    file_path,
+    JSON.stringify(config, null, '  '),
+    "utf-8"
+  );
+};
+
+main();
+
