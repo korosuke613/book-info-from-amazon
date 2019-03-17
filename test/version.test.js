5f0be3bcccde776d9709b86ea218ac4f7f167f24
Futa Hirakoba <korosuke613613@gmail.com>
2019-03-18 03:16:49 +0900

    fix

--- test/version.test.js
+++ test/version.test.js
@@ -1,6 +1,7 @@
 const update = require('../version');
 const chai = require('chai');
 const assert = chai.assert;
+const path = require('path');
 
 
 describe('version', () => {
@@ -11,6 +12,12 @@ describe('version', () => {
     it('文字列以外をはじく', () => {
       assert.strictEqual(update.load_version(5), false);
     });
+  });
+  describe('load_JSON', ()=>{
+    it('ファイルを開く', ()=>{
+      const file_path = path.resolve(__dirname, "version.test.json");
+      update.load_JSON(file_path);
+    })
   });
 });
 
