59e52476148c5104ecf37533ec66c50b2ffc466c
Futa Hirakoba <korosuke613613@gmail.com>
2019-03-24 03:00:57 +0900

    rename

--- webpack.common.js
+++ webpack.common.js
@@ -1,27 +1,21 @@
-import path from 'path';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const source_dir = 'app';
 const output_dir = 'dist';
 
-export default {
+module.exports = {
     entry: {
         content: path.join(__dirname, source_dir, 'content.ts'),
         background: path.join(__dirname, source_dir, 'background.ts'),
         popup: path.join(__dirname, source_dir, 'popup.ts'),
         options: path.join(__dirname, source_dir, 'options.ts'),
     },
-    mode: 'development',
     output: {
         path: path.join(__dirname, output_dir),
         filename: 'scripts/[name].bundle.js',
     },
     target: 'web',
-    devServer: {
-        contentBase: path.join(__dirname, output_dir),
-        port: 9000,
-        hot: true,
-    },
     module: {
         rules: [
             {
@@ -56,6 +50,5 @@ export default {
     ],
     resolve: {
         extensions: ['.ts', '.js', '.json']
-    },
-    devtool: 'cheap-module-source-map'
+    }
 };
