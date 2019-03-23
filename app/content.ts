2d51e1c40b8eafd61acef1b4de8982c630d8ec9e
Futa Hirakoba <korosuke613613@gmail.com>
2019-03-24 00:55:07 +0900

    複数ファイルも行ける

--- app/content.ts
+++ app/content.ts
@@ -1,57 +1,6 @@
 'use strict';
 
-class BookInfo {
-  title: string;
-  author: string;
-  isbn10: string = 'null';
-  isbn13: string = 'null';
-  publisher: string = 'null';
-  url: string;
-  price: number;
-  constructor() {
-    this.setTitle();
-    this.setAuthor();
-    this.setDetails();
-    this.setUrl();
-    this.setPrice();
-  }
-
-  setTitle() {
-    this.title = document.getElementById('productTitle').innerText;
-  }
-
-  setAuthor() {
-    const tmpAuthor: any = document.getElementById('bylineInfo').getElementsByClassName('a-link-normal')[0];
-    this.author = tmpAuthor.innerText.replace('のAmazon著者ページを見る', '');
-  }
-
-  setDetails() {
-    const details = document.getElementById('detail_bullets_id').getElementsByTagName('li');
-
-    for (const index in details) {
-      const text = details[index].innerText;
-      if (typeof text !== 'string') {
-        continue;
-      }
-      if (text.match(/ISBN-10/)) {
-        this.isbn10 = text;
-      }else if (text.match(/ISBN-13/)) {
-        this.isbn13 = text;
-      }else if (text.match(/出版社/)) {
-        this.publisher = text;
-      }
-    }
-  }
-
-  setUrl() {
-    this.url = document.location.href;
-  }
-
-  setPrice() {
-    const tmpPrice: any = document.getElementById('buyNewSection').getElementsByClassName('offer-price')[0];
-    this.price = tmpPrice.innerText;
-  }
-}
+import { BookInfo } from './BookInfo';
 
 let bookInfoLocal = new BookInfo();
 chrome.runtime.sendMessage(
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,18 +1,22 @@
 import path from 'path';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
+const source_dir = 'app';
+const output_dir = 'dist';
+
 export default {
     entry: {
-        content: path.join(__dirname, 'app', 'content.ts'),
+        content: path.join(__dirname, source_dir, 'content.ts'),
+        background: path.join(__dirname, source_dir, 'background.ts'),
     },
     mode: "development",
     output: {
-        path: path.join(__dirname, "dist"),
+        path: path.join(__dirname, output_dir),
         filename: 'scripts/[name].bundle.js',
     },
     target: 'web',
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        contentBase: path.join(__dirname, output_dir),
         port: 9000,
         hot: true,
     },
@@ -34,10 +38,13 @@ export default {
         new CopyWebpackPlugin(
             [
                 {
-                    from: path.join(__dirname, 'app', 'manifest.json'),
-                    to: path.join(__dirname, 'dist'),
+                    from: path.join(__dirname, source_dir, 'manifest.json'),
+                    to: path.join(__dirname, output_dir),
                 }
             ]
         )
-    ]
+    ],
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    }
 };
